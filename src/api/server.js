6934a1413816ad27c929a97d1c4761dce3692e8f
Alex Herbert <Alex-Herbert@live.nmit.ac.nz>
2024-06-03 11:45:39 +1200

    Started work on API to handle data transfer between Front end UI and MongoDB, created API directory, installed express, mongoose, mongodb, cors, body-parser dependancies. Set up JSON schema and data model, servver for running express etc.

---
+++ src/api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+//Connect to the MongoDb database
+connectDB();
+
+//Middleware
+app.use(cors());
+app.use(express.json({ extended: false}));
+
+//Define Routes
+app.use('/api/notes', require('./routes/notes'));
+
+//Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
